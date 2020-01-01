49e83d8ddc3f6de7dd0280dcdf80c73c7f933327
pandaro <padarogames@gmail.com>
2020-01-01 22:04:37 +0100

    fix callin in AIloader, not load correctly which team is this

--- luarules/gadgets/AILoader.lua
+++ luarules/gadgets/AILoader.lua
@@ -1,13 +1,13 @@
 function gadget:GetInfo()
-return {
-	name = "ShardLua",
-	desc = "Shard by AF for Spring Lua",
-	author = "eronoobos, based on gadget by raaar, and original AI by AF",
-	date = "April 2016",
-	license = "GPL",
-	layer = 999999,
-	enabled = true,
-}
+	return {
+		name = "ShardLua",
+		desc = "Shard by AF for Spring Lua",
+		author = "eronoobos, based on gadget by raaar, and original AI by AF",
+		date = "April 2016",
+		license = "GPL",
+		layer = 999999,
+		enabled = true,
+	}
 end
 
 local teams = Spring.GetTeamList()
@@ -87,306 +87,343 @@ end
 
 --SYNCED CODE
 if (gadgetHandler:IsSyncedCode()) then
-
-function gadget:Initialize()
-	GG.AiHelpers.Start()
-	local numberOfmFAITeams = 0
-	local teamList = spGetTeamList()
-	spEcho( "k9: ailoader gadget go!")
-
-	for i=1,#teamList do
-		local id = teamList[i]
-		local _,_,_,isAI,side,allyId = spGetTeamInfo(id,false)
-
-		--spEcho("Player " .. teamList[i] .. " is " .. side .. " AI=" .. tostring(isAI))
-
-		---- adding AI
-		spEcho( "K9: Is AI?")
-		if (isAI) then
-			spEcho( "K9: IT IS AI")
-			local aiInfo = spGetTeamLuaAI(id)
-			if (type(aiInfo) == "string") and (string.sub(aiInfo,1,3) == "DAI") then
-				numberOfmFAITeams = numberOfmFAITeams + 1
-				spEcho("Moomin Player " .. teamList[i] .. " is " .. aiInfo)
-				-- add AI object
-				local mode = string.sub(aiInfo, 5)
-				if DAIlist[mode] == true then
-					thisAI = ShardAI(mode)
-				else
-					Spring.Echo("ERROR : Unknown DAI mode: "..mode..". Please stop the game and restart with another DAI mode")
-					break
-				end
-				thisAI.id = id
-				thisAI.allyId = allyId
-				-- thisAI:Init()
-				AIs[#AIs+1] = thisAI
-				Shard.AIsByTeamID[id] = thisAI
-			else
-				spEcho("Player " .. teamList[i] .. " is another type of lua AI!")
-			end
-		else
-			spEcho( "K9: IS NOT AI!?")
-		end
-	end
-
-	-- add allied teams for each AI
-	for _,thisAI in ipairs(AIs) do
-		alliedTeamIds = {}
-		enemyTeamIds = {}
+	
+	function gadget:Initialize()
+		GG.AiHelpers.Start()
+		local numberOfmFAITeams = 0
+		local teamList = spGetTeamList()
+		spEcho( "k9: ailoader gadget go!")
+		
 		for i=1,#teamList do
-			if (spAreTeamsAllied(thisAI.id,teamList[i])) then
-				alliedTeamIds[teamList[i]] = true
+			local id = teamList[i]
+			local _,_,_,isAI,side,allyId = spGetTeamInfo(id,false)
+			
+			--spEcho("Player " .. teamList[i] .. " is " .. side .. " AI=" .. tostring(isAI))
+			
+			---- adding AI
+			spEcho( "K9: Is AI?")
+			if (isAI) then
+				spEcho( "K9: IT IS AI")
+				local aiInfo = spGetTeamLuaAI(id)
+				if (type(aiInfo) == "string") and (string.sub(aiInfo,1,3) == "DAI") then
+					numberOfmFAITeams = numberOfmFAITeams + 1
+					spEcho("Moomin Player " .. teamList[i] .. " is " .. aiInfo)
+					-- add AI object
+					local mode = string.sub(aiInfo, 5)
+					if DAIlist[mode] == true then
+						thisAI = ShardAI(mode)
+					else
+						Spring.Echo("ERROR : Unknown DAI mode: "..mode..". Please stop the game and restart with another DAI mode")
+						break
+					end
+					thisAI.id = id
+					thisAI.allyId = allyId
+					-- thisAI:Init()
+					AIs[#AIs+1] = thisAI
+					Shard.AIsByTeamID[id] = thisAI
+				else
+					spEcho("Player " .. teamList[i] .. " is another type of lua AI!")
+				end
 			else
-				enemyTeamIds[teamList[i]] = true
+				spEcho( "K9: IS NOT AI!?")
 			end
-		end
-		-- spEcho("AI "..thisAI.id.." : allies="..#alliedTeamIds.." enemies="..#enemyTeamIds)
-		thisAI.alliedTeamIds = alliedTeamIds
-		thisAI.enemyTeamIds = enemyTeamIds
-		thisAI.ownUnitIds = thisAI.ownUnitIds or {}
-		thisAI.friendlyUnitIds = thisAI.friendlyUnitIds or {}
-		thisAI.alliedUnitIds = thisAI.alliedUnitIds or {}
-		thisAI.enemyUnitIds = thisAI.enemyUnitIds or {}
-	end
-
-	-- catch up to started game
-	if Spring.GetGameFrame() > 1 then
-		self:GameStart()
-		-- catch up to current units
-		for _,uId in ipairs(spGetAllUnits()) do
-			self:UnitCreated(uId, Spring.GetUnitDefID(uId), Spring.GetUnitTeam(uId))
-			self:UnitFinished(uId, Spring.GetUnitDefID(uId), Spring.GetUnitTeam(uId))
-		end
-	end
-end
-
-function gadget:GameStart()
-	-- Initialise AIs
-	for _,thisAI in ipairs(AIs) do
-		local _,_,_,isAI,side = spGetTeamInfo(thisAI.id,false)
-		thisAI.side = side
-		local x,y,z = spGetTeamStartPosition(thisAI.id)
-		thisAI.startPos = {x,y,z}
-		if not thisAI.modules then thisAI:Init() end
-	end
-end
-
-
-function gadget:GameFrame(n)
-
-	-- for each AI...
-	for _,thisAI in ipairs(AIs) do
-
-		-- update sets of unit ids : own, friendlies, enemies
-		-- run AI game frame update handlers
-		prepareTheAI(thisAI)
-		thisAI:Update()
-	end
-end
-
-
-function gadget:UnitCreated(unitId, unitDefId, teamId, builderId)
-	-- for each AI...
-	local unit = Shard:shardify_unit(unitId)
-	for _,thisAI in ipairs(AIs) do
-		if (spGetUnitTeam(unitId) == thisAI.id) then
-			thisAI.ownUnitIds[unitId] = true
-			thisAI.friendlyUnitIds[unitId] = true
-		elseif (thisAI.alliedTeamIds[spGetUnitTeam(unitId)] or spGetUnitTeam(unitId) == thisAI.id) then
-			thisAI.alliedUnitIds[unitId] = true
-			thisAI.friendlyUnitIds[unitId] = true
-		else
-			thisAI.enemyUnitIds[unitId] = true
 		end
 		
-		if Spring.GetUnitTeam(unitId) == thisAI.id then
-			prepareTheAI(thisAI)
-			thisAI:UnitCreated(unit)
-		end
-		-- thisAI:UnitCreated(unitId, unitDefId, teamId, builderId)
-	end
-end
-
-function gadget:UnitDestroyed(unitId, unitDefId, teamId, attackerId, attackerDefId, attackerTeamId)
-	-- for each AI...
-	local unit = Shard:shardify_unit(unitId)
-	if unit then
+		-- add allied teams for each AI
 		for _,thisAI in ipairs(AIs) do
-			prepareTheAI(thisAI)
-			thisAI:UnitDead(unit)
-			thisAI.ownUnitIds[unitId] = nil
-			thisAI.friendlyUnitIds[unitId] = nil
-			thisAI.alliedUnitIds[unitId] = nil
-			thisAI.enemyUnitIds[unitId] = nil
-			-- thisAI:UnitDestroyed(unitId, unitDefId, teamId, attackerId, attackerDefId, attackerTeamId)
+			alliedTeamIds = {}
+			enemyTeamIds = {}
+			for i=1,#teamList do
+				if (spAreTeamsAllied(thisAI.id,teamList[i])) then
+					alliedTeamIds[teamList[i]] = true
+				else
+					enemyTeamIds[teamList[i]] = true
+				end
+			end
+			-- spEcho("AI "..thisAI.id.." : allies="..#alliedTeamIds.." enemies="..#enemyTeamIds)
+			thisAI.alliedTeamIds = alliedTeamIds
+			thisAI.enemyTeamIds = enemyTeamIds
+			thisAI.ownUnitIds = thisAI.ownUnitIds or {}
+			thisAI.friendlyUnitIds = thisAI.friendlyUnitIds or {}
+			thisAI.alliedUnitIds = thisAI.alliedUnitIds or {}
+			thisAI.enemyUnitIds = thisAI.enemyUnitIds or {}
 		end
-		Shard:unshardify_unit(self.engineUnit)
+		
+		-- catch up to started game
+		if Spring.GetGameFrame() > 1 then
+			self:GameStart()
+			-- catch up to current units
+			for _,uId in ipairs(spGetAllUnits()) do
+				self:UnitCreated(uId, Spring.GetUnitDefID(uId), Spring.GetUnitTeam(uId))
+				self:UnitFinished(uId, Spring.GetUnitDefID(uId), Spring.GetUnitTeam(uId))
+			end
+		end
 	end
-end
-
-
-function gadget:UnitDamaged(unitId, unitDefId, unitTeamId, damage, paralyzer, weaponDefId, projectileId, attackerId, attackerDefId, attackerTeamId)
-	-- for each AI...
-	local unit = Shard:shardify_unit(unitId)
-	if unit then
-		local attackerUnit = Shard:shardify_unit(attackerId)
-		local damageObj = Shard:shardify_damage(damage, weaponDefId, paralyzer)
+	
+	function gadget:GameStart()
+		-- Initialise AIs
 		for _,thisAI in ipairs(AIs) do
-			prepareTheAI(thisAI)
-			thisAI:UnitDamaged(unit, attackerUnit, damageObj)
-			-- thisAI:UnitDamaged(unitId, unitDefId, unitTeamId, attackerId, attackerDefId, attackerTeamId)
+			local _,_,_,isAI,side = spGetTeamInfo(thisAI.id,false)
+			thisAI.side = side
+			local x,y,z = spGetTeamStartPosition(thisAI.id)
+			thisAI.startPos = {x,y,z}
+			if not thisAI.modules then thisAI:Init() end
 		end
 	end
-end
-
-function gadget:UnitIdle(unitId, unitDefId, teamId)
-	-- for each AI...
-	local unit = Shard:shardify_unit(unitId)
-	if unit then
+	
+	
+	function gadget:GameFrame(n)
+		
+		-- for each AI...
 		for _,thisAI in ipairs(AIs) do
+			
+			-- update sets of unit ids : own, friendlies, enemies
+			-- run AI game frame update handlers
 			prepareTheAI(thisAI)
-			thisAI:UnitIdle(unit)
-			-- thisAI:UnitIdle(unitId, unitDefId, teamId)
+			thisAI:Update()
 		end
 	end
-end
-
-
-function gadget:UnitFinished(unitId, unitDefId, teamId)
-	-- for each AI...
-	local unit = Shard:shardify_unit(unitId)
-	if unit then
+	
+	
+	function gadget:UnitCreated(unitId, unitDefId, teamId, builderId)
+		-- for each AI...
+		local unit = Shard:shardify_unit(unitId)
 		for _,thisAI in ipairs(AIs) do
-			-- thisAI:UnitFinished(unitId, unitDefId, teamId)
-			prepareTheAI(thisAI)
-			thisAI:UnitBuilt(unit)
+			if (spGetUnitTeam(unitId) == thisAI.id) then
+				thisAI.ownUnitIds[unitId] = true
+				thisAI.friendlyUnitIds[unitId] = true
+			elseif (thisAI.alliedTeamIds[spGetUnitTeam(unitId)] or spGetUnitTeam(unitId) == thisAI.id) then
+				thisAI.alliedUnitIds[unitId] = true
+				thisAI.friendlyUnitIds[unitId] = true
+			else
+				thisAI.enemyUnitIds[unitId] = true
+			end
+			
+			if Spring.GetUnitTeam(unitId) == thisAI.id then
+				prepareTheAI(thisAI)
+				thisAI:UnitCreated(unit)
+			end
+			-- thisAI:UnitCreated(unitId, unitDefId, teamId, builderId)
 		end
 	end
-end
-
-function gadget:UnitTaken(unitId, unitDefId, teamId, newTeamId)
-	local unit = Shard:shardify_unit(unitId)
-	if unit then
-		for _,thisAI in ipairs(AIs) do
-			prepareTheAI(thisAI)
-			-- thisAI:UnitTaken(unitId, unitDefId, teamId, newTeamId)
-			thisAI:UnitDead(unit)
+	
+	function gadget:UnitDestroyed(unitId, unitDefId, teamId, attackerId, attackerDefId, attackerTeamId)
+		-- for each AI...
+		local unit = Shard:shardify_unit(unitId)
+		if unit then
+			
+			for _,thisAI in ipairs(AIs) do
+				prepareTheAI(thisAI)
+				if teamId == thisAI.id then
+					thisAI:UnitDead(unit)
+				elseif thisAI.alliedTeamIds[teamId] then
+					-- thisAI:AllyUnitDead(unit)
+				else
+					-- thisAI:EnemyUnitDead(unit)
+				end
+				thisAI.ownUnitIds[unitId] = nil
+				thisAI.friendlyUnitIds[unitId] = nil
+				thisAI.alliedUnitIds[unitId] = nil
+				thisAI.enemyUnitIds[unitId] = nil
+				-- thisAI:UnitDestroyed(unitId, unitDefId, teamId, attackerId, attackerDefId, attackerTeamId)
+			end
+			Shard:unshardify_unit(self.engineUnit)
 		end
 	end
-end
-
-function gadget:UnitGiven(unitId, unitDefId, teamId, oldTeamId)
-	local unit = Shard:shardify_unit(unitId)
-	if unit then
-		for _,thisAI in ipairs(AIs) do
-			prepareTheAI(thisAI)
-			-- thisAI:UnitCreated(unitId, unitDefId, teamId, oldTeamId)
-			thisAI:UnitCreated(unit)
+	
+	
+	function gadget:UnitDamaged(unitId, unitDefId, unitTeamId, damage, paralyzer, weaponDefId, projectileId, attackerId, attackerDefId, attackerTeamId)
+		-- for each AI...
+		local unit = Shard:shardify_unit(unitId)
+		if unit then
+			local teamId = unitTeamId
+			local attackerUnit = Shard:shardify_unit(attackerId)
+			local damageObj = Shard:shardify_damage(damage, weaponDefId, paralyzer)
+			for _,thisAI in ipairs(AIs) do
+				prepareTheAI(thisAI)
+				if teamId == thisAI.id then
+					thisAI:UnitDamaged(unit, attackerUnit, damageObj)
+				elseif thisAI.alliedTeamIds[teamId] then
+					-- thisAI:AllyUnitDamaged(unit, attackerUnit, damageObj)
+				else
+					-- thisAI:UnitDamaged(unitId, unitDefId, unitTeamId, attackerId, attackerDefId, attackerTeamId)
+				end
+			end
 		end
 	end
-end
-
-function gadget:FeatureDestroyed(featureID)
-	Shard:unshardify_feature(featureID)
-end
-
-function gadget:GameID(gameID)
-	if Shard then
-		Shard.gameID = gameID
-		local rseed = 0
-		local unpacked = VFS.UnpackU8(gameID, 1, string.len(gameID))
-		for i, part in ipairs(unpacked) do
-			-- local mult = 256 ^ (#unpacked-i)
-			-- rseed = rseed + (part*mult)
-			rseed = rseed + part
+	
+	function gadget:UnitIdle(unitId, unitDefId, teamId)
+		-- for each AI...
+		local unit = Shard:shardify_unit(unitId)
+		if unit then
+			for _,thisAI in ipairs(AIs) do
+				prepareTheAI(thisAI)
+				if teamId == thisAI.id then
+					thisAI:UnitIdle(unit)
+				elseif thisAI.alliedTeamIds[teamId] then
+					-- thisAI:AllyUnitIdle(unit)
+				else
+					-- thisAI:UnitIdle(unitId, unitDefId, teamId)
+				end
+			end
 		end
-		-- Spring.Echo("randomseed", rseed)
-		Shard.randomseed = rseed
 	end
-end
-
-
-
---UNSYNCED CODE
+	
+	
+	function gadget:UnitFinished(unitId, unitDefId, teamId)
+		-- for each AI...
+		local unit = Shard:shardify_unit(unitId)
+		if unit then
+			for _,thisAI in ipairs(AIs) do
+				-- thisAI:UnitFinished(unitId, unitDefId, teamId)
+				
+				prepareTheAI(thisAI)
+				if teamId == thisAI.id then
+					thisAI:UnitBuilt(unit)
+				elseif thisAI.alliedTeamIds[teamId] then
+					-- thisAI:AllyUnitBuilt(unit)
+				else
+					-- thisAI:EnemyUnitBuilt(unit)
+				end
+			end
+		end
+	end
+	
+	function gadget:UnitTaken(unitId, unitDefId, teamId, newTeamId)
+		local unit = Shard:shardify_unit(unitId)
+		if unit then
+			for _,thisAI in ipairs(AIs) do
+				prepareTheAI(thisAI)
+				-- thisAI:UnitTaken(unitId, unitDefId, teamId, newTeamId)
+				if teamId == thisAI.id then
+					thisAI:UnitDead(unit)
+				elseif thisAI.alliedTeamIds[teamId] then
+					-- thisAI:AllyUnitDead(unit)
+				else
+					-- thisAI:EnemyUnitDead(unit)
+				end
+			end
+		end
+	end
+	
+	function gadget:UnitGiven(unitId, unitDefId, teamId, oldTeamId)
+		local unit = Shard:shardify_unit(unitId)
+		if unit then
+			for _,thisAI in ipairs(AIs) do
+				prepareTheAI(thisAI)
+				-- thisAI:UnitCreated(unitId, unitDefId, teamId, oldTeamId)
+				if teamId == thisAI.id then
+					thisAI:UnitGiven(unit)
+				elseif thisAI.alliedTeamIds[teamId] then
+					-- thisAI:AllyUnitGiven(unit)
+				else
+					-- thisAI:EnemyUnitGiven(unit)
+				end
+			end
+		end
+	end
+	
+	function gadget:FeatureDestroyed(featureID)
+		Shard:unshardify_feature(featureID)
+	end
+	
+	function gadget:GameID(gameID)
+		if Shard then
+			Shard.gameID = gameID
+			local rseed = 0
+			local unpacked = VFS.UnpackU8(gameID, 1, string.len(gameID))
+			for i, part in ipairs(unpacked) do
+				-- local mult = 256 ^ (#unpacked-i)
+				-- rseed = rseed + (part*mult)
+				rseed = rseed + part
+			end
+			-- Spring.Echo("randomseed", rseed)
+			Shard.randomseed = rseed
+		end
+	end
+	
+	
+	
+	--UNSYNCED CODE
 else
 	local function sdAddRectangle(_, x1, z1, x2, z2, r, g, b, a, label, filled, teamID, channel)
 		if (Script.LuaUI('ShardDrawAddRectangle')) then
 			Script.LuaUI.ShardDrawAddRectangle(x1, z1, x2, z2, {r, g, b, a}, label, filled, teamID, channel)
 		end
 	end
-
+	
 	local function sdEraseRectangle(_, x1, z1, x2, z2, r, g, b, a, label, filled, teamID, channel)
 		if (Script.LuaUI('ShardDrawEraseRectangle')) then
 			Script.LuaUI.ShardDrawEraseRectangle(x1, z1, x2, z2, {r, g, b, a}, label, filled, teamID, channel)
 		end
 	end
-
+	
 	local function sdAddCircle(_, x, z, radius, r, g, b, a, label, filled, teamID, channel)
 		if (Script.LuaUI('ShardDrawAddCircle')) then
 			Script.LuaUI.ShardDrawAddCircle(x, z, radius, {r, g, b, a}, label, filled, teamID, channel)
 		end
 	end
-
+	
 	local function sdEraseCircle(_, x, z, radius, r, g, b, a, label, filled, teamID, channel)
 		if (Script.LuaUI('ShardDrawEraseCircle')) then
 			Script.LuaUI.ShardDrawEraseCircle(x, z, radius, {r, g, b, a}, label, filled, teamID, channel)
 		end
 	end
-
+	
 	local function sdAddLine(_, x1, z1, x2, z2, r, g, b, a, label, arrow, teamID, channel)
 		if (Script.LuaUI('ShardDrawAddLine')) then
 			Script.LuaUI.ShardDrawAddLine(x1, z1, x2, z2, {r, g, b, a}, label, arrow, teamID, channel)
 		end
 	end
-
+	
 	local function sdEraseLine(_, x1, z1, x2, z2, r, g, b, a, label, arrow, teamID, channel)
 		if (Script.LuaUI('ShardDrawEraseLine')) then
 			Script.LuaUI.ShardDrawEraseLine(x1, z1, x2, z2, {r, g, b, a}, label, arrow, teamID, channel)
 		end
 	end
-
+	
 	local function sdAddPoint(_, x, z, r, g, b, a, label, teamID, channel)
 		if (Script.LuaUI('ShardDrawAddPoint')) then
 			Script.LuaUI.ShardDrawAddPoint(x, z, {r, g, b, a}, label, teamID, channel)
 		end
 	end
-
+	
 	local function sdErasePoint(_, x, z, r, g, b, a, label, teamID, channel)
 		if (Script.LuaUI('ShardDrawErasePoint')) then
 			Script.LuaUI.ShardDrawErasePoint(x, z, {r, g, b, a}, label, teamID, channel)
 		end
 	end
-
+	
 	local function sdAddUnit(_, unitID, r, g, b, a, label, teamID, channel)
 		if (Script.LuaUI('ShardDrawAddUnit')) then
 			Script.LuaUI.ShardDrawAddUnit(unitID, {r, g, b, a}, label, teamID, channel)
 		end
 	end
-
+	
 	local function sdEraseUnit(_, unitID, r, g, b, a, label, teamID, channel)
 		if (Script.LuaUI('ShardDrawEraseUnit')) then
 			Script.LuaUI.ShardDrawEraseUnit(unitID, {r, g, b, a}, label, teamID, channel)
 		end
 	end
-
+	
 	local function sdClearShapes(_, teamID, channel)
 		if (Script.LuaUI('ShardDrawClearShapes')) then
 			Script.LuaUI.ShardDrawClearShapes(teamID, channel)
 		end
 	end
-
+	
 	local function sdDisplay(_, onOff)
 		if (Script.LuaUI('ShardDrawDisplay')) then
 			Script.LuaUI.ShardDrawDisplay(onOff)
 		end
 	end
-
+	
 	local function sStartTimer(_, name)
 		if (Script.LuaUI('ShardStartTimer')) then
 			Script.LuaUI.ShardStartTimer(name)
 		end
 	end
-
+	
 	local function sStopTimer(_, name)
 		if (Script.LuaUI('ShardStopTimer')) then
 			Script.LuaUI.ShardStopTimer(name)
@@ -418,19 +455,19 @@ else
 		gadgetHandler:AddSyncAction('ShardStopTimer', sStopTimer)
 		gadgetHandler:AddSyncAction('ShardSaveTable', sSaveTable)
 	end
-
+	
 	function gadget:Shutdown()
 		Spring.Echo("Shard AI unsync gadget shutdown")
 		gadgetHandler:RemoveSyncAction("shard_debug_position")
 	end
-
+	
 	function handleShardDebugPosEvent(_,x,z,col)
-	--	Spring.Echo("handleShardDebugPosEvent 1")
+		--	Spring.Echo("handleShardDebugPosEvent 1")
 		if Script.LuaUI("shard_debug_position") then
 			Spring.Echo("handleShardDebugPosEvent 2")
 			Script.LuaUI.shard_debug_position(x,z,col)
 			Spring.Echo("handleShardDebugPosEvent 3")
 		end
 	end
-
+	
 end
